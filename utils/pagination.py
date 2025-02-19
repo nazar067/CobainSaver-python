fa6a44f66d0732a70f990ef26d23cd3280218995
KnownNight <kasich.nszar@gmail.com>
2025-02-19 12:47:36 +0200

    update for linux

--- utils/pagination.py
+++ utils/pagination.py
@@ -3,18 +3,21 @@ from downloader.playlist import process_music_playlist
 
 
 async def playlist_pagination(callback: CallbackQuery, dp):
-    _, source, content_type, playlist_id, new_page = callback.data.split()
-    chat_id = callback.message.chat.id
-    msg_id = callback.message.message_id
-    business_connection_id = callback.message.business_connection_id
-    print(source)
-    print(content_type)
-    if source == "Y":
-        await process_music_playlist(callback.bot, dp, business_connection_id, int(chat_id), f"https://music.youtube.com/playlist?list={playlist_id}", int(new_page), int(msg_id))
-    elif source == "S":
-        if content_type is "a":
-            await process_music_playlist(callback.bot, dp, business_connection_id, int(chat_id), f"https://open.spotify.com/album/{playlist_id}", int(new_page), int(msg_id))
-        elif content_type is "p":
-            print("pagination")
-            await process_music_playlist(callback.bot, dp, business_connection_id, int(chat_id), f"https://open.spotify.com/playlist/{playlist_id}", int(new_page), int(msg_id))
-    await callback.answer()
+    try:
+        _, source, content_type, playlist_id, new_page = callback.data.split()
+        chat_id = callback.message.chat.id
+        msg_id = callback.message.message_id
+        business_connection_id = callback.message.business_connection_id
+        print(source)
+        print(content_type)
+        if source == "Y":
+            await process_music_playlist(callback.bot, dp, business_connection_id, int(chat_id), f"https://music.youtube.com/playlist?list={playlist_id}", int(new_page), int(msg_id))
+        elif source == "S":
+            if content_type is "a":
+                await process_music_playlist(callback.bot, dp, business_connection_id, int(chat_id), f"https://open.spotify.com/album/{playlist_id}", int(new_page), int(msg_id))
+            elif content_type is "p":
+                print("pagination")
+                await process_music_playlist(callback.bot, dp, business_connection_id, int(chat_id), f"https://open.spotify.com/playlist/{playlist_id}", int(new_page), int(msg_id))
+        await callback.answer()
+    except Exception as e:
+        print(e)
