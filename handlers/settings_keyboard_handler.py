8b9259399744dba926a61d44a35bef4c03874c27
KnownNight <kasich.nszar@gmail.com>
2025-02-19 19:08:15 +0200

    fix ads button in private messages

--- handlers/settings_keyboard_handler.py
+++ handlers/settings_keyboard_handler.py
@@ -10,10 +10,11 @@ async def toggle_audio_callback(callback: CallbackQuery, dp):
     pool = dp["db_pool"]
     chat_id = int(chat_id)
     new_state = bool(int(new_state))
+    business_connection_id = callback.message.business_connection_id
 
     await upsert_settings(pool, chat_id, send_tiktok_music=new_state)
 
-    updated_keyboard = await generate_settings_keyboard(chat_id, new_state, send_ads=True, pool=pool)
+    updated_keyboard = await generate_settings_keyboard(chat_id, new_state, send_ads=True, pool=pool, business_connection_id=business_connection_id)
     await callback.message.edit_reply_markup(reply_markup=updated_keyboard)
 
 async def toggle_ads_callback(bot: Bot, message: Message, dp):
