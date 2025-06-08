da70019da095d7acc459280a31df8ab93505791e
Kasich Nazar <nazar067@MacBook-Air-Kasich.local>
2025-06-09 00:35:34 +0300

    fix linux

--- utils/fetch_data.py
+++ utils/fetch_data.py
@@ -25,8 +25,8 @@ async def fetch_youtube_data(url: str, user_folder: str, quality: str) -> dict:
         'merge_output_format': 'mp4',
         'noplaylist': True,
         'quiet': True,
-        'cookiefile': 'cookies.txt',
-        'cookies_from_browser': None, 
+        # 'cookiefile': 'cookies.txt',
+        # 'cookies_from_browser': None, 
         'http_headers': {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.115 Safari/537.36'},
     }
 
