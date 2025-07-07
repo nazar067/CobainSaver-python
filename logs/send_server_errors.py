6aed7bbba1cda262f09e338b23310b52f7e8f28d
KnownNight <kasich.nszar@gmail.com>
2025-07-07 16:24:34 +0300

    hot fix send log from users in private chats

--- logs/send_server_errors.py
+++ logs/send_server_errors.py
@@ -1,6 +1,6 @@
 import os
 from aiogram import Bot
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.types.input_file import FSInputFile
 from aiogram.exceptions import TelegramBadRequest
 from config import LOGS_ID
@@ -37,24 +37,26 @@ async def send_server_logs(message: Message, dp):
     except TelegramBadRequest as e:
         await message.reply(server_translation["logs"][user_language].format(e=e))
         
-async def send_log_from_users(bot: Bot, callback, dp):
+async def send_log_from_users(bot: Bot, callback: CallbackQuery, dp):
     try:
         pool = dp["db_pool"]
         data = callback.data.split()
         file_path = data[1]
         chat_id = callback.message.chat.id
+        business_connection_id = callback.message.business_connection_id
         chat_language = await get_language(pool, chat_id)
         
         if not os.path.exists(file_path):
             if callback.message.chat.type == "private":    
                 await callback.answer(error_translation["old_error"][chat_language], show_alert=True)
             else: 
-                await callback.bot.send_message(chat_id=chat_id, text=error_translation["old_error"][chat_language])
+                await callback.bot.send_message(chat_id=chat_id, text=error_translation["old_error"][chat_language], business_connection_id=business_connection_id)
                 
             await bot.edit_message_reply_markup(
                 chat_id=callback.message.chat.id,
                 message_id=callback.message.message_id,
-                reply_markup=None
+                reply_markup=None,
+                business_connection_id=business_connection_id
             )
             return
         
@@ -74,7 +76,8 @@ async def send_log_from_users(bot: Bot, callback, dp):
         await bot.edit_message_reply_markup(
             chat_id=callback.message.chat.id,
             message_id=callback.message.message_id,
-            reply_markup=None
+            reply_markup=None,
+            business_connection_id=business_connection_id
         )
     except Exception as e:
         print(e)
